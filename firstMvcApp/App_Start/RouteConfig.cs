af38069ee8082ad1a63076222c34f77800f16154
Vishnu Pillai <Vishnu Pillai>
2016-04-10 16:54:10 -0500

    no message

--- firstMvcApp/App_Start/RouteConfig.cs
+++ firstMvcApp/App_Start/RouteConfig.cs
@@ -12,12 +12,13 @@ namespace firstMvcApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.MapRoute("Name, url, controller and action")
+            routes.MapRoute("Login", "login", new { Controller="Auth", Action="Login" });
+            routes.MapRoute("Home","", new {Controller="Posts", Action="Index" });
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+
+  
+
         }
     }
 }
---
+++ firstMvcApp/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstMvcApp.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Users!");
+        }
+    }
+}
